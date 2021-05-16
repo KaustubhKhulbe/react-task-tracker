6c86367077c2456e2b681503083aa6353477ed80
kaustubh <kaustubh.khulbe@gmail.com>
2021-05-16 11:52:56 -0700

    tracker finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react"
 import {useState, useEffect} from "react"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
+import About from "./components/About"
 
 function App() {
   const[showAddTask, setShowAddTask] = useState(false)
@@ -75,7 +78,7 @@ const toggleReminder = async (id) => {
     body: JSON.stringify(updTask)
   })
 
-  const data = await res.json
+  const data = await res.json()
 
   console.log(data.reminder);
 
@@ -84,11 +87,20 @@ const toggleReminder = async (id) => {
 }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}></AddTask>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks to show"}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        <Route path="/" exact render={(props) => (
+          <> 
+          {showAddTask && <AddTask onAdd={addTask}></AddTask>}
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks to show"}
+          </>
+        )}
+        />
+        <Route path="/about" component={About}/>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import Button from "./Button"
+import {useLocation} from "react-router-dom"
 
 const Header = ({title, onAdd, showAdd}) => {
 
@@ -7,10 +8,14 @@ const Header = ({title, onAdd, showAdd}) => {
           console.log("clicked");
      }
 
+     const location = useLocation()
+
      return (
           <header className="header">
                <h1>{title}</h1>
+               {location.pathname === "/" &&
                <Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd}/>
+               }
           </header>
      )
 }
